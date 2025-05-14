44a53902bd8e1edf96c15c32715e3ce9e064ff6b
smiley405 <smileypiratesgame@gmail.com>
2025-05-14 16:00:53 +0100

    fix(nvim): remove depricated diagnostic sign_define

--- nvim/lua/config/diagnostic.lua
+++ nvim/lua/config/diagnostic.lua
@@ -1,23 +1,30 @@
-local signs = { Error = 'E ', Warn = 'W ', Hint = 'H ', Info = 'I ' }
-
-for type, icon in pairs(signs) do
-	local hl = 'DiagnosticSign' .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-end
-
 vim.diagnostic.config({
-	virtual_text = false,
-	update_in_insert = false,
-	severity_sort = true,
-	float = {
-	  focused = false,
-	  style = 'minimal',
-	  border = 'rounded',
-	  source = 'always',
-	  header = '',
-	  prefix = '',
-	},
-})
+		virtual_text = false,
+		update_in_insert = false,
+		severity_sort = true,
+		float = {
+			focused = false,
+			style = 'minimal',
+			border = 'rounded',
+			source = 'always',
+			header = '',
+			prefix = '',
+		},
+		signs = {
+			text = {
+				[vim.diagnostic.severity.ERROR] = 'E',
+				[vim.diagnostic.severity.WARN] = 'W',
+				[vim.diagnostic.severity.INFO] = 'I',
+				[vim.diagnostic.severity.HINT] = 'H',
+			},
+			numhl = {
+				[vim.diagnostic.severity.ERROR] = '',
+				[vim.diagnostic.severity.WARN] = '',
+				[vim.diagnostic.severity.HINT] = '',
+				[vim.diagnostic.severity.INFO] = '',
+			},
+		},
+	})
 
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
